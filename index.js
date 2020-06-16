6a68beff75de51a1960d1bb111f46b5a82bd2f41
Dylan Awalt-Conley <web@dylan.ac>
2020-06-16 12:47:18 -0400

    simplified module code a bit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const path = require('path');
 const p = (...args) => path.join(__dirname, ...args);
-
 const parser = require(p('parser.js'));
+
+function promisifyKwargs (kwargs) {
+    const asyncKwargs = Object.entries(kwargs).map(e => Promise.all(e));
+    return Promise.all(asyncKwargs)
+        .then(entries =>
+            entries.reduce((obj, [key, val]) => {
+                obj[key] = val;
+                return obj;
+            }, {}));
+}
 
 module.exports = function (args, evalFunc) {
     if (evalFunc)
@@ -12,23 +21,16 @@ module.exports = function (args, evalFunc) {
         return results;
 
     let arg1 = results[0]
-
     if (arg1 && arg1.__keywords === true)
         arg1 = arg1[Object.keys(arg1).find(k => k !== '__keywords')];
 
     if (arg1 instanceof Promise) {
-        results = results.map(arg => {
-            if (arg.__keywords === true) {
-                let asyncKwargs = Object.entries(arg).map(e => Promise.all(e));
-                return Promise.all(asyncKwargs)
-                    .then(kwargs =>
-                        kwargs.reduce((obj, [key, val]) => {
-                            obj[key] = val;
-                            return obj;
-                        }, {}));
+        for (let i = 0; i < results.length; i++) {
+            if (results[i].__keywords === true) {
+                results[i] = promisifyKwargs(results[i]);
+                break;
             }
-            return arg; // don't need to do this in a map. just find the kwargs if it exists
-        });
+        }
         results = Promise.all(results);
     }
 
