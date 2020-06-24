087b8499963b0ab82d57b4c61bd477ff5dbdf439
Dylan Awalt-Conley <web@dylan.ac>
2020-06-24 18:20:39 -0400

    updated arg separator to accept commas without any whitespace. updated tests to include this situation and a few other cases of odd spacing.

--- test.js
+++ test.js
@@ -46,15 +46,15 @@ const tests = [
     {
         input: [
             [ `{% test "O'Malley", age=68 %}` ],
-            [ `{% test 'O\\'Malley', age=68.0 %}` ],
-            [ `{% test name, age=age %}`, {name:"O'Malley",age:68} ],
-            [ `{% test person.name, age=person.age %}`, {person:{name:"O'Malley",age:68}} ],
+            [ `{% test 'O\\'Malley',    age=68.0 %}` ],
+            [ `{% test name,   age=age %}`, {name:"O'Malley",age:68} ],
+            [ `{% test person.name,age=person.age %}`, {person:{name:"O'Malley",age:68}} ],
         ],
         output: `["O'Malley",{"__keywords":true,"age":68}]`
     },
     {
         input: [
-            [ `{% test name="O'Malley", age=68 %}` ],
+            [ `{% test name="O'Malley",\nage=68 %}` ],
             [ `{% test name='O\\'Malley' age=68.0 %}` ],
             [ `{% test name=name, age=age %}`, {name:"O'Malley",age:68} ],
             [ `{% test name=person.name age=person.age %}`, {person:{name:"O'Malley",age:68}} ],
@@ -63,9 +63,9 @@ const tests = [
     },
     {
         input: [
-            [ `{% test 'foo', 'bar', "baz" name="O'Malley", age=68 %}` ],
-            [ `{% test 'foo' "bar" baz name='O\\'Malley' age=68.0 %}`, {baz:'baz'} ],
-            [ `{% test a1, a2, a3, name=name, age=age %}`, {a1:'foo',a2:'bar',a3:'baz',name:"O'Malley",age:68} ],
+            [ `{% test 'foo',\r'bar',\n"baz"    name="O'Malley", age=68 %}` ],
+            [ `{% test 'foo'\r\r\r"bar" baz name='O\\'Malley' age=68.0 %}`, {baz:'baz'} ],
+            [ `{% test a1,a2,a3,name=name,age=age %}`, {a1:'foo',a2:'bar',a3:'baz',name:"O'Malley",age:68} ],
             [ `{% test a.foo a.bar a.baz name=person.name age=person.age %}`, {a:{foo:'foo',bar:'bar',baz:'baz'},person:{name:"O'Malley",age:68}} ],
         ],
         output: `["foo","bar","baz",{"__keywords":true,"name":"O'Malley","age":68}]`
